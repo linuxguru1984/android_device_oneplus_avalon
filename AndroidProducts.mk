2e7312e830fd103c817add526b37f843c2a43e43
Alex Freeman <linuxguru51984@gmail.com>
2025-07-20 17:04:59 +0300

    move repo to gitlab

--- AndroidProducts.mk
+++
@@ -1,7 +0,0 @@
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_avalon.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_avalon-eng \
-    twrp_avalon-userdebug \
-    twrp_avalon-user
--- vendorsetup.sh
+++
@@ -1,3 +0,0 @@
-#add_lunch_combo twrp_avalon-eng
-#add_lunch_combo twrp_avalon-userdebug
-#add_lunch_combo twrp_avalon-user
